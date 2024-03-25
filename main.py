c11cd16e0c36ed516f159f9752a65fb3c78cf9ae
AngWilliamLee <160611372+AngWilliamLee@users.noreply.github.com>
2024-03-25 15:18:52 +0000

    first try

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
-def to_hms(seconds: int) -> list:
-    """
-    Converts seconds to hours, minutes, and seconds, and returns it as a list.
+seconds = input("Enter the number of seconds: ")
 
-    Parameters
-    ---------
-    seconds: int
-        the seconds to be calculated
+if not all(char in '0123456789' for char in str(seconds)):
+  print("error1")
+else:
+  def to_hms(seconds: int) -> list:
+      """
+      Converts seconds to hours, minutes, and seconds, and returns it as a list.
+  
+      Parameters
+      ---------
+      seconds: int
+          the seconds to be calculated
+  
+      Returns
+      ---------
+      list
+          a list of integers representing hours, minutes, seconds
+  
+      Example:
+      >>> to_hms(10)
+      [0, 0, 10]
+      >>> to_hms(61)
+      [0, 1, 1]
+      >>> to_hms(7199)
+      [1, 59, 59]
+      """
+  
+      # if not all(char in '0123456789' for char in str(seconds)):
+      #   print("error1")
+      # else:
+      seconds = int(seconds)
+      hours = seconds // 3600
+      minutes = (seconds % 3600) // 60
+      remaining_seconds = seconds % 60
+      hms = [hours, minutes, remaining_seconds]
+      return hms
 
-    Returns
-    ---------
-    list
-        a list of integers representing hours, minutes, seconds
+print(to_hms(seconds))
+  
+ 
 
-    Example:
-    >>> to_hms(10)
-    [0, 0, 10]
-    >>> to_hms(61)
-    [0, 1, 1]
-    >>> to_hms(7199)
-    [1, 59, 59]
-    """
-    # Type your code below
-    pass
+
+
+  
+  
+
+
+
 
